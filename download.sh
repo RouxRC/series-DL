ed3a6d1910871a432cfb622d5e4d405138df0629
RouxRC <b.ooghe@gmail.com>
2016-01-10 02:56:55 +0100

    better formatting for sources

--- download.sh
+++ download.sh
@@ -4,9 +4,6 @@ cd $(dirname $0)
 source config.inc
 ROOT_URL="https://kat.cr/usearch/"
 
-# TODO
-# should take a number of pages per source
-
 mkdir -p .tmp
 touch episodes.done
 
@@ -46,38 +43,47 @@ echo
 echo $(date)
 echo "----------------------"
 echo "$SOURCES" | while read SOURCE; do
-  QUERY=$(echo "$SOURCE" | sed -r "s| *([0-9]+)?$|%20${RES}p/\1|" | sed -r 's|(/[0-9]+)$|\1/|')
-  URL="${ROOT_URL}${QUERY}?field=time_add&sorder=desc"
-  echo "QUERY $URL"
-  curl -sL "$URL" | zcat                          |
-   grep 'class="cellMainLink"\|\.torrent?title'   |
-   tr '\n' ' '                                    |
-   sed 's|//torcache|\nhttp://torcache|g'         |
-   sed 's|?title=.*class="cellMainLink">|#|'      |
-   sed 's|</a>.*$||'                              |
-   sed 's|<\/\?[^>]*>||g'                         |
-   while read line; do
-    TORRENT_URL=$(echo "$line" | sed 's/#.*$//')
-    TORRENT_ID=$(echo "$TORRENT_URL" | sed -r 's|^.*/([0-9A-F]+)\.torrent|\1|')
-    TORRENT_NAME=$(echo "$line" | sed 's/^.*#//')
-    TORRENT_EP=$(echo "$TORRENT_NAME"                   |
-     sed -r "s/ ${RES}p .*$//"                          |
-     sed -r 's/\(?[0-9]{4}\)? (S[0-9]+E[0-9]+)/\1/i'    |
-     sed -r 's/(E[0-9]+) [a-z]+ [a-z]+.*$/\1/i')
-    SEARCHABLE=$(uniqname "$TORRENT_NAME")
-    LOWERED=$(lowerize "$TORRENT_EP")
-    if grep "$LOWERED" episodes.done > /dev/null; then
-      continue
-    elif $DL_ALL_FIRST_EPS && echo "$TORRENT_EP" | grep -i " S01E01" > /dev/null; then
-      start_torrent
-      continue
-    fi
-    echo "$SHOWS" | while read SHOW; do
-      MATCH=$(uniqname "$SHOW")
-      if echo "$SEARCHABLE" | grep "^$MATCH.*s[0-9]\+e[0-9]\+" > /dev/null; then
+  SOURCE=$(echo "$SOURCE" | sed 's/\s\+/ /g' | sed -r 's/(^ | $)//')
+  PAGES=1
+  if echo "$SOURCE" | grep " [0-9]\+\s*" > /dev/null; then
+    PAGES=$(echo "$SOURCE" | sed -r 's/^.* ([0-9]+) ?$/\1/')
+  fi
+  QUERY=$(echo "$SOURCE"        |
+   sed -r "s| +[0-9]+?$||"      |
+   sed 's/ /%20/g')"%20${RES}p"
+  for PAGE in $(seq $PAGES); do
+    URL="${ROOT_URL}$QUERY/$PAGE/?field=time_add&sorder=desc"
+    echo "QUERY $URL"
+    curl -sL "$URL" | zcat                          |
+     grep 'class="cellMainLink"\|\.torrent?title'   |
+     tr '\n' ' '                                    |
+     sed 's|//torcache|\nhttp://torcache|g'         |
+     sed 's|?title=.*class="cellMainLink">|#|'      |
+     sed 's|</a>.*$||'                              |
+     sed 's|<\/\?[^>]*>||g'                         |
+     while read line; do
+      TORRENT_URL=$(echo "$line" | sed 's/#.*$//')
+      TORRENT_ID=$(echo "$TORRENT_URL" | sed -r 's|^.*/([0-9A-F]+)\.torrent|\1|')
+      TORRENT_NAME=$(echo "$line" | sed 's/^.*#//')
+      TORRENT_EP=$(echo "$TORRENT_NAME"                   |
+       sed -r "s/ ${RES}p .*$//"                          |
+       sed -r 's/\(?[0-9]{4}\)? (S[0-9]+E[0-9]+)/\1/i'    |
+       sed -r 's/(E[0-9]+) [a-z]+ [a-z]+.*$/\1/i')
+      SEARCHABLE=$(uniqname "$TORRENT_NAME")
+      LOWERED=$(lowerize "$TORRENT_EP")
+      if grep "$LOWERED" episodes.done > /dev/null; then
+        continue
+      elif $DL_ALL_FIRST_EPS && echo "$TORRENT_EP" | grep -i " S01E01" > /dev/null; then
         start_torrent
-        break
+        continue
       fi
+      echo "$SHOWS" | while read SHOW; do
+        MATCH=$(uniqname "$SHOW")
+        if echo "$SEARCHABLE" | grep "^$MATCH.*s[0-9]\+e[0-9]\+" > /dev/null; then
+          start_torrent
+          break
+        fi
+      done
     done
   done
 done
